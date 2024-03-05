0bec7c2294794b5cb5edefad155afbe753db2de0
Ryan Balisi <ryan@copperpress.tech>
2024-03-05 17:53:31 +0800

    added one to many relationship example

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Category;
+use App\Models\Item;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,15 +22,33 @@ Route::get('/', function () {
 
 Route::get('/categories', function () {
 
-    $categories = Category::where('name', 'components')->get();
+    $categories = Category::all();
     // Select * from categories where name = 'components'
 
     foreach($categories as $category) {
         echo 'ID: ' . $category->id . 'Name: ' . $category->name . '<br>';
+        echo 'items: ' .'list of items <br>';
+        $items = $category->items()->get();
+        // print_r($items);
+        foreach($items as $item) {
+            echo 'items: ' . $item->name;
+        }
     }
 
     // print_r(Category::all());
 });
+
+Route::get('/items', function () {
+
+    $items = Item::all();
+
+    foreach($items as $item) {
+        echo 'ID: ' . $item->id . 'Name: ' . $item->name . '<br>';
+    }
+
+    // print_r(Category::all());
+});
+
 
 Route::get('/login', function () {
 
